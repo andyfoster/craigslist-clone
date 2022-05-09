93744b6d0c273cb9540f282665ab846fcf4c824e
Andy Foster <andrewfosteris@gmail.com>
2022-05-09 18:15:03 +1200

    add posts and categories controllers

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_27_042126) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_03_023027) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,6 +20,15 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_27_042126) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "parent_id"
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.bigint "category_id"
+    t.string "title"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_posts_on_category_id"
   end
 
 end
